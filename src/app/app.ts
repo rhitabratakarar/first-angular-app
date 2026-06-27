5e97757a3871c0677979badbfadc37bb3b6b9bba
rhitabratakarar <karar_rhitabrata@outlook.com>
2026-06-27 09:43:45 +0530

    added routing and details page.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
 import { Home } from './home/home';
-import { NgOptimizedImage } from "@angular/common";
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   template: ` <main>
-    <header class="brand-name">
-      <img class="brand-logo" [ngSrc]="logoPath" aria-hidden="true" aria-hidden="true" width="150" height="60">
-    </header>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          [ngSrc]="logoPath"
+          aria-hidden="true"
+          aria-hidden="true"
+          width="150"
+          height="60"
+        />
+      </header>
+    </a>
     <section class="content">
-      <app-home></app-home>
+      <router-outlet />
     </section>
   </main>`,
   styleUrls: ['./app.css'],
-  imports: [Home, NgOptimizedImage],
+  imports: [Home, NgOptimizedImage, RouterLink, RouterOutlet],
 })
 export class App {
-  logoPath: string = "logo.svg";
+  logoPath: string = 'logo.svg';
 }
---
+++ src/app/routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Details } from './details/details';
+
+const routeConfig: Routes = [
+  {
+    path: '',
+    component: Home,
+    title: 'Home page',
+  },
+  {
+    path: 'details/:id',
+    component: Details,
+    title: 'Home details',
+  },
+  {
+    path: '**',
+    redirectTo: '', // fallback
+  },
+];
+
+export default routeConfig;
