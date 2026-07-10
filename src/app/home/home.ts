02888c00d0a743ebbea8a4827ad5189a595a673f
rhitabratakarar <karar_rhitabrata@outlook.com>
2026-07-10 08:06:17 +0530

    search functionality

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -9,12 +9,12 @@ import { HousingLocationInfo } from '../housinglocation';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
-      @for (housingLocation of housingLocationList; track $index) {
+      @for (housingLocation of filteredLocationList; track $index) {
         <app-housing-location [housingLocation]="housingLocation" />
       }
     </section>
@@ -23,9 +23,22 @@ import { HousingLocationInfo } from '../housinglocation';
 })
 export class Home {
   housingLocationList: HousingLocationInfo[] = [];
+  filteredLocationList: HousingLocationInfo[] = [];
   housingService: Housing = inject(Housing);
 
   constructor() {
     this.housingLocationList = this.housingService.housingLocationList;
+    this.filteredLocationList = this.housingService.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    } else {
+      this.filteredLocationList = this.housingLocationList.filter((x) =>
+        x.city.toLowerCase().includes(text.toLowerCase()),
+      );
+    }
   }
 }
